59f97efb2bc0e68115621c4767e298e85ce0eacf
dareldoudy <86224686+dareldoudy@users.noreply.github.com>
2022-10-16 17:19:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet">
+        <title>Accueil - Blog PHP</title>
+    </head>
+    <body>
+        <h1>Page d'accueil</h1>
+        <form action="action.php" method="post" class="creation">
+            <input type="text" class="input-titre" name="titre">
+            <input type="text" class="input-contenu" name="contenu">
+            <input type="submit" value="Générer un article" name=bouton>
+        </form>
+        <p>
+           c'est une page web en php dans le cadre d'un TP 
+        </p>
+    </body>
+</html>
